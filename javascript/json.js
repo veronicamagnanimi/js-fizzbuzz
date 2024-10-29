6ec4790c1355005656095bc656fa7446a2e5291f
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-10-29 14:29:44 +0100

    feat: exercise completed

--- javascript/json.js
+++ javascript/json.js
@@ -1,1 +1,24 @@
-alert("ciao");
+//ciclo
+for (let i = 1; i <= 100; i++) {
+
+
+// se divisibile per 3   
+    if (i % 3 === 0) {
+        console.log("Fizz");
+    } 
+
+// se divisibile per 5  
+    else if (i % 5 === 0) {
+        console.log("Buzz");
+    }
+
+// se divisibile per entrambi
+    else if ((i % 3 === 0) && (i % 5 === 0)) {
+        console.log("FizzBuzz");
+    }
+
+// in tutti gli altri casi
+    else {
+       console.log(i);
+    }
+}
